235f5ec2098cca720983b4d90ac666c6200964b5
sm.yoo <sm.yoo@testworks.co.kr>
2022-05-03 11:54:02 +0900

    docs: 설명 변경

--- src/utils/Protected.tsx
+++ src/utils/Protected.tsx
@@ -9,7 +9,7 @@ export interface ProtectedProps {
 }
 
 /**
- * 로그인한 유저에게만 경로 허용 결정
+ * 로그인한 유저에게만 하위 컴포넌트 렌더링
  * @param {boolean} auth 로그인 여부, true면 허용 false면 redirect
  * @param {React.ReactNode|React.ReactNode[]} children 컴포넌트
  * @param {string} redirectPath 로그인 X 일때 리다이렉트 경로
