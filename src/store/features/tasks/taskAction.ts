10407807b306b72c708127984fecca22cc36aae9
sm.yoo <sm.yoo.work@gmail.com>
2022-07-13 18:04:14 +0900

    fix: 전체 데이터 목록 > 삭제 기능 추가 완료

--- src/store/features/tasks/taskAction.ts
+++ src/store/features/tasks/taskAction.ts
@@ -2,6 +2,7 @@ import { PayloadAction } from '@reduxjs/toolkit';
 import { Task } from '../../../utils/nia15/interfaces/tasks';
 
 export interface TaskState {
+  method: string | null;
   time: string | null;
   totalCount: number;
   taskList: Task[];
@@ -9,6 +10,7 @@ export interface TaskState {
 }
 
 export const initialState: TaskState = {
+  method: null,
   time: null,
   totalCount: 0,
   taskList: [],
@@ -17,29 +19,37 @@ export const initialState: TaskState = {
 
 const taskAction = {
   assign: (state: TaskState) => {
+    state.method = 'POST';
     state.taskList = [];
     state.totalCount = 0;
   },
   getTaskList: (state: TaskState) => {
+    state.method = 'GET';
     state.taskList = [];
   },
   setTaskList: (state: TaskState, action: PayloadAction<Task[]>) => {
+    state.method = 'GET';
     state.taskList = action.payload;
   },
   setWorkTask: (state: TaskState, action: PayloadAction<Task | null>) => {
     state.workTask = action.payload;
   },
   getWorkTask: (state: TaskState, action: PayloadAction<number>) => {
+    state.method = 'GET';
     state.workTask = null;
   },
   setCount: (state: TaskState, action: PayloadAction<number>) => {
     state.totalCount = action.payload;
   },
   getExpiredAt: (state: TaskState) => {
+    state.method = 'GET';
     state.time = null;
   },
   setTime: (state: TaskState, action: PayloadAction<string>) => {
     state.time = action.payload;
+  },
+  deleteTask: (state: TaskState, action: PayloadAction<number>) => {
+    state.method = 'DELETE';
   },
 };
 
